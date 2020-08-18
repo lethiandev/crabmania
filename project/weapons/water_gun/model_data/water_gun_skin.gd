9ec5d3cf2b7a195426fcbb92a404afed210f68eb
Konrad Nowakowski <konrad.x92@gmail.com>
2020-08-18 13:24:45 +0100

    Add gun rotating towards cursor

--- project/weapons/water_gun/model_data/water_gun_skin.gd
+++ project/weapons/water_gun/model_data/water_gun_skin.gd
@@ -1,8 +1,27 @@
 extends Spatial
 
+var target: Vector3 = Vector3.ZERO
+
 func _process(p_delta: float) -> void:
-	var bone_idx = $Armature/Skeleton.find_bone("arm")
-	var xform = $Armature/Skeleton.get_bone_pose(bone_idx)
-	var new_xform = xform.rotated(Vector3.FORWARD, p_delta)
+	_update_rotation(target)
+
+func _update_rotation(p_target: Vector3) -> void:
+	var look_normal = (global_transform.origin - p_target).normalized()
+	var bone_basis = Transform().looking_at(look_normal, Vector3.UP).basis
 	
-	$Armature/Skeleton.set_bone_pose(bone_idx, new_xform)
+	# Clamp rotation
+	var euler = bone_basis.get_euler()
+	euler.x = clamp(euler.x, -deg2rad(45), deg2rad(10))
+	bone_basis = Basis(euler)
+	
+	# Fix gun orientation
+	bone_basis *= Basis(Vector3.RIGHT, PI * 0.5)
+	
+	_update_bone_rotation("arm", bone_basis)
+
+func _update_bone_rotation(p_bone: String, p_basis: Basis) -> void:
+	var bone_idx = $Armature/Skeleton.find_bone(p_bone)
+	var bone_xform = $Armature/Skeleton.get_bone_global_pose(bone_idx)
+	bone_xform.basis = bone_xform.basis.slerp(p_basis, 0.1)
+	
+	$Armature/Skeleton.set_bone_global_pose_override(bone_idx, bone_xform, 1.0)
--- project/weapons/water_gun/water_gun.gd
+++ project/weapons/water_gun/water_gun.gd
@@ -1,16 +1,24 @@
 extends Spatial
 
+var last_cursor: Vector2 = Vector2.ZERO
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+func _input(p_event: InputEvent) -> void:
+	if p_event is InputEventMouseMotion:
+		last_cursor = p_event.position
 
+func _process(p_delta: float) -> void:
+	var target = _cast_screen_ray(last_cursor)
+	if target != Vector3.ZERO:
+		_update_look_target(target)
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _cast_screen_ray(p_cursor: Vector2) -> Vector3:
+	var camera = get_viewport().get_camera()
+	var space = get_world().get_direct_space_state()
+	var from = camera.project_ray_origin(p_cursor)
+	var to = camera.project_ray_normal(p_cursor) * 100.0
+	
+	var result = space.intersect_ray(from, to, [])
+	return Vector3.ZERO if result.empty() else result["position"] 
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _update_look_target(p_target: Vector3) -> void:
+	$WaterGunSkin.target = p_target
