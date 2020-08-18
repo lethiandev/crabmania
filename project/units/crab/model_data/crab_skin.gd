28a18e99c4b0d62f4e6838930e577e624c314799
Konrad Nowakowski <konrad.x92@gmail.com>
2020-08-18 13:30:21 +0100

    Add center ground normal checking

--- project/units/crab/model_data/crab_skin.gd
+++ project/units/crab/model_data/crab_skin.gd
@@ -7,10 +7,12 @@ func _update_ground() -> void:
 	var n1 = _ground_normal(Vector3(0.163, 0.0, 0.121))
 	var n2 = _ground_normal(Vector3(-0.163, 0.0, 0.121))
 	var n3 = _ground_normal(Vector3(0.0, 0.0, -0.228))
+	var n4 = _ground_normal(Vector3(0.0, 0.0, 0))
 	
-	var dir = (n1 + n2 + n3).normalized()
+	var dir = (n1 + n2 + n3 + n4).normalized()
 	var new_xform = Transform().looking_at(dir, Vector3.BACK)
-	transform = transform.interpolate_with(new_xform * Transform().rotated(Vector3(1.0, 0.0, 0.0), -PI * 0.5), 0.1)
+	var rotated = Transform().rotated(Vector3(1.0, 0.0, 0.0), -PI * 0.5)
+	transform = transform.interpolate_with(new_xform * rotated, 0.1)
 
 func _ground_normal(p_offset: Vector3) -> Vector3:
 	var space = get_world().direct_space_state
